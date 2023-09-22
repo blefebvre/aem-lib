f5707df89522aaad84e8b438c6dad7b310370e22
Bruce Lefebvre <blefebvr@adobe.com>
2023-09-22 11:52:53 -0400

    Enable getMetadata to query any document

--- src/dom-utils.js
+++ src/dom-utils.js
@@ -59,11 +59,12 @@ export async function loadScript(src, attrs) {
 /**
  * Retrieves the content of metadata tags.
  * @param {string} name The metadata name (or property)
+ * @param {Document} doc Document object to query for metadata. Defaults to the window's document
  * @returns {string} The metadata value(s)
  */
-export function getMetadata(name) {
+export function getMetadata(name, doc = document) {
   const attr = name && name.includes(':') ? 'property' : 'name';
-  const meta = [...document.head.querySelectorAll(`meta[${attr}="${name}"]`)].map((m) => m.content).join(', ');
+  const meta = [...doc.head.querySelectorAll(`meta[${attr}="${name}"]`)].map((m) => m.content).join(', ');
   return meta || '';
 }
 
